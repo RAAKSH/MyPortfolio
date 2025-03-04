3fe964e5a08daeefed2a28313558f146c237438f
Raksha <rakshac29@gmail.com>
2025-03-04 11:37:57 +0530

    Image issue resolved

--- src/Components/constants.tsx
+++ src/Components/constants.tsx
@@ -1,14 +1,18 @@
+import BoxedBite from "../assets/BoxedBite.png";
+import weather from "../assets/WeatherApp.png";
+
+
 export const projects = [{
-    id:1,
+    id: 1,
     name: "Boxed Bite",
     desc: "A fully responsive food delivery app seamlessly integrating Swiggy’s live API. Built from the ground up using React, this app leverages modern web development techniques, ensuring a smooth user experience and optimized performance.",
-    img: "src/assets/BoxedBite.png"
+    img: BoxedBite
 },
 {
-    id:2,
+    id: 2,
     name: "Weather Forecast",
     desc: "The Weather App is a React-based web application that provides real-time weather updates for any city worldwide. It utilizes the OpenWeather API to fetch weather data and displays key details like temperature, humidity, wind speed. This app  ensures a dynamic and responsive user experience.",
-    img: "src/assets/WeatherApp.png"
-},
+    img: weather
+}
 
 ]
