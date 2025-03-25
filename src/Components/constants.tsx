4b157880e528506964d194d682290d995add40a7
Raksha <rakshac29@gmail.com>
2025-03-25 11:01:50 +0530

    Added filmflex GPT

--- src/Components/constants.tsx
+++ src/Components/constants.tsx
@@ -1,5 +1,6 @@
 import BoxedBite from "../assets/BoxedBite.png";
 import weather from "../assets/WeatherApp.png";
+import filmflex from "../assets/filmflex.png";
 
 
 export const projects = [{
@@ -8,11 +9,18 @@ export const projects = [{
     desc: "A fully responsive food delivery app seamlessly integrating Swiggy’s live API. Built from the ground up using React, this app leverages modern web development techniques, ensuring a smooth user experience and optimized performance.",
     img: BoxedBite
 },
+
 {
     id: 2,
+    name: "FilmflexGPT",
+    desc: "FilmflexGPT is a  Netflix clone with GPT-3 integration. It includes firebase authentication,leveraging data from the TMDB API to provide a vast library of movie. And also sophisticated movie recommendation system by integrating the OpenAI API, enabling the app to generate personalized recommendations based on user preferences. Additionally, introduced multilingual features to cater to adiverse user base. ",
+    img: filmflex
+},
+{
+    id: 3,
     name: "Weather Forecast",
     desc: "The Weather App is a React-based web application that provides real-time weather updates for any city worldwide. It utilizes the OpenWeather API to fetch weather data and displays key details like temperature, humidity, wind speed. This app  ensures a dynamic and responsive user experience.",
     img: weather
-}
+},
 
 ]
